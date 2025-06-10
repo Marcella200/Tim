5da51cb7d85106acd5989a3d883b94f613a05808
Marcella <hesgiag@gmail.com>
2025-06-10 21:37:03 +0700

    Menambahkan test login baru

--- cypress/e2e/lms_login.cy.js
+++ cypress/e2e/lms_login.cy.js
@@ -1,6 +1,6 @@
-describe('Authentication Test', () =>
+describe('Authentication Test', () => {
   beforeEach(() => {
-    cy.visit('https://lms.teknologidigital.co.id/login');
+    cy.visit('https://lms.teknologidigital.co.id/login')
   });
 
  it('TC-LOGIN-01: Admin berhasil login dengan email dan kata sandi yang benar', () => {
@@ -10,4 +10,27 @@ describe('Authentication Test', () =>
     cy.contains('Dashboard').should('be.visible');
     cy.contains(/hesgiag@gmail\.com/i).should('be.visible');
   });
+});
+
+   it('TC-LOGIN-02: Admin login dengan email kosong', () => {
+    cy.get('input[name="email"]').should('be.visible').type('leave empty');
+    cy.get('input[name="password"]').should('be.visible').type('monokuma');
+    cy.get('button[type="submit"]').click();
+    cy.contains(/Email atau Kata Sandi Tidak Sesuai/i).should('be.visible');
+  });
+
+  it('TC-LOGIN-03: Admin login dengan password kosong', () => {
+    cy.get('input[name="email"]').type('hesgiag@gmai.com');
+    cy.get('input[name="password"]').type('leave empty');
+    cy.get('button[type="submit"]').click();
+    cy.contains('Email atau Kata Sandi Tidak Sesuai.').should('be.visible');
+  });
+
+  it('TC-LOGIN-04: Admin login dengan password minimal 8 karakter', () => {
+    cy.get('input[name="email"]').type('hesgiag@gmai.com');
+    cy.get('input[name="password"]').type('password');
+    cy.get('button[type="submit"]').click();
+    cy.contains('Email atau Kata Sandi Tidak Sesuai.').should('be.visible');
+  });
+
   
