76a0fb294e9b0760c1db05cb5a4e5e777c1a60ab
Marcella <hesgiag@gmail.com>
2025-06-12 00:52:33 +0700

    melanjutkan kerjaan dashboard test

--- cypress/e2e/lms_dash.cy.js
+++ cypress/e2e/lms_dash.cy.js
@@ -1,23 +1,26 @@
 describe('Authentication Test', () => {
   beforeEach(() => {
     cy.visit('https://lms.teknologidigital.co.id/login')
+    cy.get('input[placeholder="Alamat Email"]').type('hesgiag@gmail.com');
+    cy.get('input[placeholder="Kata Sandi"]').type('monokuma');
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', '/panel');
   });
 
    it('TC-DSH-01: Membuka halaman Dashboard', () => {
-    cy.get('input[type="Alamat Email"]').type('hesgiag@gmail.com');
-    cy.get('input[type="Kata Sandi"]').type('monokuma');
-    cy.get('button[type="submit"]').click();
-    cy.url().should('include', '/panel');
     cy.contains('Dashboard').should('be.visible');
   });
 
    it('TC-DSH-02: Dropdown pada halaman Dashboard', () => {
-    cy.get('button.dropdownTrigger').should('be.visible');
-    cy.get('button.dropdownTrigger').click();
-    cy.get('.dropdownMenu').should('be.visible');
+    cy.get('span.overflow-hidden.truncate.max-w-xs')
+    .should('contain', 'hesgiag@gmail.com')
+    .click();
     cy.contains('Sign out').should('be.visible');
-  });
+});
 
    it('TC-DSH-03: Mengakses halaman Dashboard menggunakan URL', () => {
+    cy.visit('https://lms.teknologidigital.co.id/panel'); 
+    cy.url().should('include', '/panel'); 
+    cy.contains('Dashboard').should('be.visible');
+  });
   });
-});
