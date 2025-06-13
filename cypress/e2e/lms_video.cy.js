1cafe9d92eb9d152d36c69e8523f4ee34638f56d
Marcella <hesgiag@gmail.com>
2025-06-14 01:40:42 +0700

    melanjutkan kerjaan video test

--- cypress/e2e/lms_video.cy.js
+++ cypress/e2e/lms_video.cy.js
@@ -1,28 +1,47 @@
-describe('Halaman Daftar Gallery', () => {
+describe('Halaman Dashboard', () => {
   beforeEach(() => {
-    cy.visit('https://lms.teknologidigital.co.id/login');
-    cy.get('input[type="email"]').type('hesgiag@gmail.com');
-    cy.get('input[type="password"]').type('monokuma');
+    cy.visit('https://lms.teknologidigital.co.id/login')
+    cy.get('input[placeholder="Alamat Email"]').type('hesgiag@gmail.com');
+    cy.get('input[placeholder="Kata Sandi"]').type('monokuma');
     cy.get('button[type="submit"]').click();
     cy.url().should('include', '/panel');
   });
 
-  it('Buka halaman Video dan cek elemen', () => {
-  cy.contains('Video').click();
+  it('#TC-VID-01 - Melihat Halaman Video', () => {
+    cy.contains('Video').click();
+    cy.url().should('include', '/panel/video');
+    cy.get('input[name="name"]').should('exist');
+    cy.get('input[name="youtube_id"]').should('exist');
   });
 
-  it('should fill the video form and preview the YouTube video', () => {
-    cy.get('input[name="name"]').clear().type('Marcella')
+  it('#TC-VID-02 - Form Isian pada halaman Video', () => {
+  cy.contains('Video').click();
+  cy.get('input[name="name"]')
+    .should('be.visible')
+    .clear()
+    .should('have.value', '');    
+  cy.get('input[name="youtube_id"]').should('be.visible');
+  cy.get('iframe').should('exist');
+  });
 
+  it('#TC-VID-03 - Upload Video Baru', () => {
+    const name = 'Sakya';
+    const youtubeUrl = 'https://www.youtube.com/watch?v=PT2_F-1esPk&list=RDMM&index=19';
 
-    const youtubeUrl = 'https://www.youtube.com/watch?v=sT-wpjhYy-0';
-
+    cy.contains('Video').click();
+    cy.get('input[name="name"]').clear().type(name);
     cy.get('input[name="youtube_id"]').clear().type(youtubeUrl);
-
     cy.contains('Simpan').click();
-
+    cy.contains('Berhasil memperbaharui video!').should('be.visible');
     cy.get('iframe')
       .should('have.attr', 'src')
-      .and('include', 'sT-wpjhYy-0');
-  });
+      .and('include', 'https://www.youtube.com/watch?v=PT2_F-1esPk&list=RDMM&index=19');
+  });
+
+  it('#TC-VID-04 - Dropdown Halaman Video', () => {
+    cy.get('span.overflow-hidden.truncate.max-w-xs')
+    .should('contain', 'hesgiag@gmail.com')
+    .click();
+    cy.contains('Sign out').should('be.visible');
+  });
 });
