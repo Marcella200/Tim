3cdffa92ac7bd93a7365509983b3e1292d8c768d
Marcella <hesgiag@gmail.com>
2025-06-14 01:18:24 +0700

    melanjutkan kerjaan gallery test

--- cypress/e2e/lms_gall.cy.js
+++ cypress/e2e/lms_gall.cy.js
@@ -1,14 +1,50 @@
-describe('LMS Login Test', () => {
-  it('Login berhasil dengan email dan password valid', () => {
-    cy.visit('https://lms.teknologidigital.co.id/login');
-
-    cy.get('input[type="email"]').type('hesgiag@gmail.com');
-
-    cy.get('input[type="password"]').type('monokuma');
-
+describe('Halaman Gallery', () => {
+  beforeEach(() => {
+    cy.visit('https://lms.teknologidigital.co.id/login')
+    cy.get('input[placeholder="Alamat Email"]').type('hesgiag@gmail.com');
+    cy.get('input[placeholder="Kata Sandi"]').type('monokuma');
     cy.get('button[type="submit"]').click();
-
     cy.url().should('include', '/panel');
-    cy.contains('Dashboard').should('be.visible');
-  });
+  });
+
+  it('TC-GAL-01: Membuka halaman Gallery', () => {
+    cy.contains('Gallery').should('be.visible').click();
+    cy.url().should('include', '/panel/gallery');
+    cy.contains(/daftar gallery/i).should('be.visible');
+    cy.contains('Nama').should('be.visible');
+    cy.contains('Gambar').should('be.visible');
+    cy.contains('Dibuat').should('be.visible');
+    cy.contains('Aksi').should('be.visible');
+    cy.contains('Data belum tersedia.').should('be.visible');
+  });
+
+  it('TC-GAL-02: Menambahkan Foto', () => {
+    cy.contains('Gallery').should('be.visible').click();
+    cy.url().should('include', '/panel/gallery');
+    cy.contains('a', '+ Foto', { timeout: 10000 }).should('be.visible').click(); 
+    cy.url().should('include', '/panel/gallery/create');
+    cy.get('input[name="name"]').type('Marcella');
+    cy.get('input[type="file"]').selectFile('cypress/fixtures/contoh.jpg');
+    cy.contains('Simpan').click();  
+    cy.url().should('include', '/panel/gallery');
+});
+
+  it('TC-GAL-03: Menghapus foto yang baru saja di tambahkan', () => {
+    cy.contains('Gallery').click();
+    cy.url().should('include', '/panel/gallery');
+    cy.contains('td', 'Marcella')
+      .parent('tr')
+      .within(() => {
+    cy.get('button').contains(/^$/).click({ force: true });
+    });
+
+    cy.on('window:confirm', () => true);
+  });
+
+    it('TC-GAL-04: Dropdown Halaman Gallery', () => {
+    cy.get('span.overflow-hidden.truncate.max-w-xs')
+    .should('contain', 'hesgiag@gmail.com')
+    .click();
+    cy.contains('Sign out').should('be.visible');
+  });
 });
