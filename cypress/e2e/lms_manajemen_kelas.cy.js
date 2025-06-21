7b029cd2c9f07db90d7a28d176d5b07979e5c62f
Marcella <hesgiag@gmail.com>
2025-06-22 00:50:43 +0700

    melanjutkan kerjaan manajemen kelas

--- cypress/e2e/lms_manajemen_kelas.cy.js
+++ cypress/e2e/lms_manajemen_kelas.cy.js
@@ -19,8 +19,8 @@ describe('Manajemen Kelas Test', () => {
   });
 
   it('#TC-MK-02 Klik tombol +Tipe Kelas', () => {
-    openManajemenKelas();
-    cy.get('a[href*="/course-types/create"]').click();
+    cy.contains('Manajemen Kelas').click();
+    cy.contains('+ Tipe Kelas').click();
     cy.url().should('include', '/panel/course-types/create');
     cy.get('input[name="name"]').type('Kelas Marcella');
     cy.contains('Simpan').click();
@@ -46,9 +46,9 @@ describe('Manajemen Kelas Test', () => {
   it('#TC-MK-05 Klik Icon Delete pada tabel', () => {
     openManajemenKelas();
     cy.contains('Kelas Marcella').parents('tr').within(() => {
-      cy.get('button.bg-red-500').click();
+    cy.get('button.bg-red-500').click();
     });
-    cy.contains('Are you sure').should('be.visible');
+    cy.contains('Are yo sure').should('exist').and('be.visible');
     cy.contains('OK').click();
     cy.get('table').should('not.contain', 'Kelas Marcella');
   });
@@ -122,15 +122,15 @@ describe('Manajemen Kelas Test', () => {
   cy.get('svg[data-icon="akar-icons:eye"]').parents('a').click(); 
   });
 
-  cy.contains('Corel Draw A').parents('tr').within(() => {
+  cy.contains('Corel Draw').parents('tr').within(() => {
   cy.get('svg[data-icon="akar-icons:eye"]').parents('a').click(); 
   });
 
   cy.get('tbody tr').each(($row) => {
   cy.wrap($row).within(() => {
-  cy.get('svg[data-icon="akar-icons:eye"]').parents('a').click(); 
-  cy.get('a').has('svg[data-icon="akar-icons:pencil"]').should('exist');
-  cy.get('button').has('svg[data-icon="akar-icons:trash-can"]').should('exist');
+      cy.get('svg[data-icon="akar-icons:eye"]').should('exist');
+      cy.get('svg[data-icon="bx:bx-edit"]').should('exist');
+      cy.get('svg[data-icon="cil:trash"]').should('exist');
     });
   });
 });
@@ -141,7 +141,7 @@ describe('Manajemen Kelas Test', () => {
   cy.get('svg[data-icon="akar-icons:eye"]').parents('a').click();
     });
 
-  cy.contains('Corel Draw A').parents('tr').within(() => {
+  cy.contains('Corel Draw').parents('tr').within(() => {
   cy.get('svg[data-icon="akar-icons:eye"]').parents('a').click();
     });
 
@@ -149,18 +149,22 @@ describe('Manajemen Kelas Test', () => {
   cy.url().should('include', '/panel/courses/17/classes/create');
 
   cy.get('input[name="name"]').type('Kelas Baru');
-  cy.get('select[name="Pilih Guru"]').select('frami.blanche@example.net');
-  cy.get('input[name="start_date"]').type('20/06/2025');
-  cy.get('input[name="end_date"]').type('20/07/2025');
-  cy.get('select[name="status"]').select('Belum Aktif');
-  cy.get('select[name="Pilih Murid"]').select('lotusmahaesa@gmail.com');
+  cy.get('.choices').eq(0).click();
+  cy.get('.choices__list--dropdown')
+  .contains('lula02@example.org (Laurie Kessler)').click();
+  cy.get('input[name="start_date"]').clear().type('2025-06-20');
+  cy.get('input[name="end_date"]').clear().type('2025-07-20');
+  cy.get('select[name="status"]').select('Belum aktif');
+  cy.get('.choices').eq(1).click();
+  cy.get('.choices__list--dropdown')
+  .contains('pacocha.bette@example.com').click();
 
   cy.contains('Simpan').click();
 
   cy.contains('Kelas berhasil ditambahkan').should('be.visible');
 });
 
-it('#TC-MK-ADM-13 Kolom Menu Pencarian (Search)', () => {
+it('#TC-MK-13 Kolom Menu Pencarian (Search)', () => {
   openManajemenKelas();
   cy.contains('Khusus').parents('tr').within(() => {
     cy.get('svg[data-icon="akar-icons:eye"]').parents('a').click();
@@ -176,14 +180,18 @@ it('#TC-MK-14 Klik tombol icon "Pensil" untuk edit Kelas', () => {
     cy.get('svg[data-icon="akar-icons:eye"]').parents('a').click();
   });
 
-  cy.contains('Corel Draw A').parents('tr').within(() => {
-    cy.get('a').has('svg[data-icon="akar-icons:pencil"]').click();
+  cy.contains('Corel Draw').parents('tr').within(() => {
+    cy.get('svg[data-icon="akar-icons:eye"]').click();
   });
 
-  cy.get('input[name="name"]').clear().type('Corel Draw X');
+    cy.contains('Marcella').parents('tr').within(() => {
+    cy.get('a[href*="/panel/classes/edit/19"]').click();
+  });
+
+  cy.get('input[name="name"]').clear().type('Marcella X');
   cy.contains('Simpan').click();
 
-  cy.contains('Mata Pelajaran berhasil diperbarui').should('be.visible');
+  cy.contains('Kelas berhasil ditambahkan.').should('be.visible');
 });
 
 it('#TC-MK-15 Klik tombol icon "Pensil" untuk edit Nama Mata Pelajaran', () => {
@@ -192,19 +200,14 @@ it('#TC-MK-15 Klik tombol icon "Pensil" untuk edit Nama Mata Pelajaran', () => {
     cy.get('svg[data-icon="akar-icons:eye"]').parents('a').click();
   });
 
-  cy.contains('Corel Draw A').parents('tr').within(() => {
-  cy.get('svg[data-icon="akar-icons:eye"]').parents('a').click();
+  cy.contains('autoCAD').parents('tr').within(() => {
+    cy.get('svg[data-icon="bx:bx-edit"]').parents('a').click();
   });
 
-  cy.contains('Corel Draw').parents('tr').within(() => {
-    cy.get('a').has('svg[data-icon="akar-icons:pencil"]').click();
-  });
-
-  cy.get('input[name="name"]').clear().type('Corel Draw S');
+  cy.get('input[name="name"]').clear().type('autoCAD Y');
   cy.contains('Simpan').click();
 
   cy.contains('Mata Pelajaran berhasil diperbarui').should('be.visible');
-  cy.get('table').should('contain', 'Corel Draw S');
 });
 
 it('#TC-MK-16 Klik tombol icon "Mata" pada Kolom Aksi Khusus', () => {
@@ -217,6 +220,10 @@ it('#TC-MK-16 Klik tombol icon "Mata" pada Kolom Aksi Khusus', () => {
     cy.get('svg[data-icon="akar-icons:eye"]').parents('a').click(); 
   });
 
+  cy.contains('Corel Draw A').parents('tr').within(() => {
+    cy.get('svg[data-icon="akar-icons:eye"]').parents('a').click(); 
+  });
+
    const detailKelasCorelDraw = [
   'Informasi Murid',
   'Informasi Kelas',
@@ -226,123 +233,128 @@ it('#TC-MK-16 Klik tombol icon "Mata" pada Kolom Aksi Khusus', () => {
   detailKelasCorelDraw.forEach((nama) => {
   cy.contains(nama).should('be.visible');
 });
-
-it('#TC-MK-17 Klik tombol Delete pada kolom Aksi', () => {
-  openManajemenKelas();
-  cy.contains('Khusus').parents('tr').within(() => {
-    cy.get('svg[data-icon="akar-icons:eye"]').parents('a').click(); 
-  });
-
-  cy.contains('Corel Draw A').parents('tr').within(() => {
-    cy.get('svg[data-icon="akar-icons:eye"]').parents('a').click(); 
-  });
-
-  cy.contains('Corel Draw').parents('tr').within(() => {
-    cy.get('svg[data-icon="akar-icons:trash-can"]').parents('button').click();
-  });
-
-  cy.contains('Are you sure').should('be.visible');
-  cy.contains('OK').click();
-  cy.get('table').should('not.contain', 'Corel Draw');
 });
 
-it('#TC-MK-18 Edit data mata pelajaran', () => {
-  openManajemenKelas();
-  cy.contains('Khusus').parents('tr').within(() => {
-    cy.get('svg[data-icon="akar-icons:eye"]').parents('a').click(); 
+ it('#TC-MK-17 Klik tombol Delete pada kolom Aksi', () => {
+    openManajemenKelas();
+    cy.contains('Khusus').parents('tr').within(() => {
+      cy.get('a').find('svg[data-icon="akar-icons:eye"]').click();
+    });
+
+    cy.contains('Corel Draw').parents('tr').within(() => {
+      cy.get('a').find('svg[data-icon="akar-icons:eye"]').click();
+    });
+
+    cy.contains('Corel Draw A').parents('tr').within(() => {
+      cy.get('svg[data-icon="cil:trash"]').click({ force: true });
+    });
+
+    cy.contains('Are you sure').should('be.visible');
+    cy.contains('OK').click();
+    cy.get('table').should('not.contain', 'Corel Draw');
   });
 
-  cy.contains('autoCAD').parents('tr').within(() => {
-    cy.get('svg[data-icon="akar-icons:pencil"]').parents('a').click();
+  it('#TC-MK-18 Edit data mata pelajaran', () => {
+    openManajemenKelas();
+    cy.contains('Khusus').parents('tr').within(() => {
+      cy.get('a').find('svg[data-icon="akar-icons:eye"]').click();
+    });
+
+    cy.contains('autoCAD Y').parents('tr').within(() => {
+      cy.get('svg[data-icon="bx:bx-edit"]').parents('a').click();
+    });
+
+    cy.get('input[name="name"]').clear().type('autoCAD Book');
+    cy.contains('Simpan').click();
+    cy.contains('Mata Pelajaran berhasil diperbarui.').should('be.visible');
+    cy.contains('autoCAD Book').should('exist');
   });
 
-  cy.get('input[name="name"]').clear().type('autoCAD Book');
-  cy.contains('Simpan').click();
-  cy.contains('Kelas berhasil ditambahkan').should('be.visible');
-  cy.contains('autoCAD Book').should('exist');
-});
+  it('#TC-MK-19 Edit nama tipe kelas Khusus', () => {
+    openManajemenKelas();
 
-it('#TC-MK-19 Edit nama tipe kelas Khusus', () => {
-  openManajemenKelas();
+    cy.contains('Khusus').parents('tr').within(() => {
+      cy.get('a[href$="/edit"]').click();
+    });
 
-  cy.contains('Khusus').parents('tr').within(() => {
-    cy.get('a[href$="/edit"]').click();
+    cy.get('input[name="name"]').clear().type('Khusus Baru');
+    cy.contains('Simpan').click();
+    cy.contains('Tipe kelas berhasil diperbarui').should('be.visible');
+    cy.contains('Khusus Baru').should('exist');
   });
-
-  cy.get('input[name="name"]').clear().type('Khusus Baru');
-  cy.contains('Simpan').click();
-  cy.contains('Tipe kelas berhasil diperbarui').should('be.visible');
-  cy.contains('Khusus Baru').should('exist');
-});
 
 // #TC-MK-ADM-03 Manajemen Kelas Admin > Daftar Mata Pelajaran Tipe Kelas Reguler
-it('#TC-MK-20 Membuka Halaman Manajemen Kelas', () => {
-  openManajemenKelas();
-  cy.contains('Daftar Tipe Kelas').should('be.visible');
-});
+ it('#TC-MK-20 Membuka Halaman Manajemen Kelas (Reguler)', () => {
+    openManajemenKelas();
+    cy.contains('Daftar Tipe Kelas').should('be.visible');
+  });
 
-it('#TC-MK-21 Menambah +Mata Pelajaran Reguler', () => {
+  it('#TC-MK-21 Menambah +Mata Pelajaran Reguler', () => {
     openManajemenKelas();
     cy.contains('Reguler').parents('tr').within(() => {
-    cy.get('svg[data-icon="akar-icons:eye"]').parents('a').click(); 
+      cy.get('a').find('svg[data-icon="akar-icons:eye"]').click();
     });
-    
+
     cy.contains('+ Mata Pelajaran').click();
-    cy.url().should('include', '/panel/course-types/1/courses/create'); 
+    cy.url().should('include', '/panel/course-types/1/courses/create');
     cy.get('input[name="name"]').type('Drawing for Kids');
     cy.contains('Simpan').click();
-    cy.contains('Mata Pelajaran berhasil ditambahkan').should('be.visible');
+    cy.contains('Mata Pelajaran berhasil ditambahkan.').should('be.visible');
   });
 
-it('#TC-MK-22 Menu pencarian nama (search)', () => {
-  openManajemenKelas();
-  cy.get('input[name="search"]').type('Reguler');
-  cy.contains('Reguler').should('be.visible');
-});
-
-it('#TC-MK-23 Klik Icon Delete pada salah satu data tabel di kolom Aksi', () => {
-  openManajemenKelas();
-  cy.contains('Reguler').parents('tr').within(() => {
-    cy.get('button.bg-red-500').click();
+  it('#TC-MK-22 Menu pencarian nama (search) tipe kelas', () => {
+    openManajemenKelas();
+    cy.get('input[name="search"]').type('Reguler');
+    cy.contains('Reguler').should('be.visible');
   });
-  cy.contains('Are you sure').should('be.visible');
-  cy.contains('OK').click();
-  cy.get('table').should('not.contain', 'Drawing for Kids');
-});
 
-it('#TC-MK-24 Cek tombol di tabel Aksi per baris', () => {
-  openManajemenKelas();
-  cy.get('tbody tr').each(($row) => {
-    cy.wrap($row).within(() => {
-      cy.get('a[href$="/edit"]').should('exist'); 
-      cy.get('a[href*="/panel/course-types/"]').should('exist'); 
+  it('#TC-MK-23 Klik Icon Delete pada salah satu data tabel di kolom Aksi', () => {
+    openManajemenKelas();
+    cy.contains('Reguler').parents('tr').within(() => {
+      cy.get('a').find('svg[data-icon="akar-icons:eye"]').click();
+    });
+
+    cy.contains('Drawing for Kids').parents('tr').within(() => {
+      cy.get('svg[data-icon="cil:trash"]').click({ force: true });
+    });
+    cy.contains('Are you sure').should('be.visible');
+    cy.contains('OK').click();
+    cy.get('table').should('not.contain', 'Drawing for Kids');
+  });
+
+  it('#TC-MK-24 Cek tombol di tabel Aksi per baris (Reguler)', () => {
+    openManajemenKelas();
+    cy.get('tbody tr').each(($row) => {
+      cy.wrap($row).within(() => {
+        cy.get('a[href$="/edit"]').should('exist');
+        cy.get('a[href*="/panel/course-types/"]').should('exist');
+      });
     });
   });
-});
 
-it('#TC-MK-25 Mengubah Tipe Kelas Reguler', () => {
-  openManajemenKelas();
-  cy.contains('Reguler r').parents('tr').within(() => {
-  cy.contains('Basic Drawing').parents('tr').within(() => {
-  cy.get('a[href$="/edit"]').click();
+  it('#TC-MK-25 Mengubah Mata Pelajaran dalam Tipe Kelas Reguler', () => {
+    openManajemenKelas();
+    cy.contains('Reguler').parents('tr').within(() => {
+      cy.get('a').find('svg[data-icon="akar-icons:eye"]').click();
+    });
+
+    cy.contains('Basic Drawing').parents('tr').within(() => {
+      cy.get('svg[data-icon="bx:bx-edit"]').parents('a, button').click({ force: true });
+    });
+
+    cy.get('input[name="name"]').clear().type('Basic Drawing Y');
+    cy.contains('Simpan').click();
+    cy.contains('Mata Pelajaran berhasil diperbarui.').should('be.visible');
   });
 
-  cy.get('input[name="name"]').clear().type('Corel Draw Updated');
-  cy.contains('Simpan').click();
-  cy.contains('Tipe kelas berhasil diperbarui').should('be.visible');
-});
+  it('#TC-MK-26 Edit nama tipe kelas Reguler', () => {
+    openManajemenKelas();
+    cy.contains('Reguler').parents('tr').within(() => {
+      cy.get('a[href$="/edit"]').click();
+    });
 
-it('#TC-MK-26 Edit nama tipe kelas Reguler', () => {
-  openManajemenKelas();
-  cy.contains('Reguler').parents('tr').within(() => {
-    cy.get('a[href$="/edit"]').click();
+    cy.get('input[name="name"]').clear().type('Reguler r');
+    cy.contains('Simpan').click();
+    cy.contains('Tipe kelas berhasil diperbarui').should('be.visible');
   });
-
-  cy.get('input[name="name"]').clear().type('Reguler r');
-  cy.contains('Simpan').click();
-  cy.contains('Tipe kelas berhasil diperbarui').should('be.visible');
-});
-});
-
-});
 });
